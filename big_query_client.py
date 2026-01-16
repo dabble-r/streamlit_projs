5a5f2917fe7a842aa08e6860695273e69a630ca6
dabble-r <njbroussard.1@gmail.com>
2026-01-15 20:47:25 -0600

    streamlit big query - text query

--- big_query_client.py
+++ big_query_client.py
@@ -2,6 +2,7 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 import streamlit as st
 import db_dtypes as dbt
+import pandas as pd
 
 
 # Construct a BigQuery client object.
@@ -19,22 +20,27 @@ def get_bigquery_client():
 
 client = get_bigquery_client()
 
-query = """
-    SELECT name, SUM(number) as total_people
-    FROM `bigquery-public-data.usa_names.usa_1910_2013`
-    WHERE state = 'TX'
-    GROUP BY name, state
-    ORDER BY total_people DESC
-    LIMIT 20
-"""
+st.title("Big Query Client")
 
-
-@st.cache_data
+#@st.cache_data
 def run_query(query: str):
     client = get_bigquery_client()
     rows = client.query_and_wait(query)  # Make an API request.
     df = rows.to_dataframe()
     return df
 
-df = run_query(query)
-st.dataframe(df)
+def query_handler(query: str):
+    if query == "":
+        st.error("Please enter a query")
+        return None
+    df = run_query(query)
+    st.dataframe(df)
+
+
+query_2 = st.text_area(label="query", value="", height="content", max_chars=None, key=None, help=None, 
+                       on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, 
+                       label_visibility="visible", width="stretch")
+
+submit_btn = st.button(label="submit", key=None, help=None, on_click=lambda: query_handler(query_2), args=None, kwargs=None, 
+                       type="secondary", icon=None, disabled=False, use_container_width=None, 
+                       width="content", shortcut=None)
